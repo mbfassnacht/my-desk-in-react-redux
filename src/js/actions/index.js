7f606f316359e3f462514ce59928b0aa1fe542cc
Maximo Belen <maximo.fassnacht@exozet.com>
2019-10-26 10:35:59 +0200

    change gh pages

--- src/js/actions/index.js
+++
@@ -1,9 +0,0 @@
-import { TURN_OFF, TURN_ON } from "../constants/action-types";
-
-export function turnOn(payload) {
-    return { type: TURN_ON, payload };
-}
-
-export function turnOff(payload) {
-    return { type: TURN_OFF, payload };
-}
--- src/js/constants/action-types.js
+++
@@ -1,2 +0,0 @@
-export const TURN_ON = "TURN_ON";
-export const TURN_OFF = "TURN_OFF";
