a13ca0c4cc19417a53047ecd740f3ce679a25efa
Maximo Belen <maximo.fassnacht@exozet.com>
2019-10-26 10:45:53 +0200

    add ribbon

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ import Desk from "./js/components/desk/Desk";
 import './index.css';
 
 render(
-  <Provider store={store}>
-    <Desk />
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <a href="https://github.com/mbfassnacht/my-desk-in-react-redux">
+            <img 
+                style={{position: 'absolute', top: 0, left: 0, border: 0}} 
+                src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png" 
+                alt="Fork me on GitHub" 
+                />
+        </a>
+        <Desk />
+    </Provider>,
+    document.getElementById("root")
 );
